b9f80826ea41032cf90949a5273c058d43fd8e81
Bahaa <bhaaahmed188@gmail.com>
2022-12-26 16:10:25 +0200

    Uploading files

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import IClip from 'src/app/models/clip.model';
+import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-manage',
@@ -8,14 +12,31 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
+  clips: IClip[] = [];
+  activeClip: IClip | null = null;
+  sort$: BehaviorSubject<string>;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService,
+    private modal: ModalService
+  ) {
+    this.sort$ = new BehaviorSubject(this.videoOrder);
+  }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(
-      (params) =>
-        (this.videoOrder = params['sort'] === '2' ? params['sort'] : '1')
-    );
+    this.route.queryParams.subscribe((params) => {
+      this.videoOrder = params['sort'] === '2' ? params['sort'] : '1';
+      this.sort$.next(this.videoOrder);
+    });
+
+    this.clipService.getUserClips(this.sort$).subscribe((docs) => {
+      this.clips = [];
+      docs.forEach((doc) => {
+        this.clips.push({ docID: doc.id, ...doc.data() });
+      });
+    });
   }
 
   sort(e: Event) {
@@ -27,5 +48,33 @@ export class ManageComponent implements OnInit {
     //   relativeTo: this.route,
     //   queryParams: { sort: value },
     // });
+  }
+
+  openModal(e: Event, clip: IClip) {
+    e.preventDefault();
+
+    this.activeClip = clip;
+
+    this.modal.toggleModal('editClip');
+  }
+
+  update(e: IClip) {
+    this.clips.forEach((el, idx) => {
+      if (el.docID == e.docID) {
+        this.clips[idx].title = e.title;
+      }
+    });
+  }
+
+  deleteClip(e: Event, clip: IClip) {
+    e.preventDefault();
+
+    this.clipService.deleteClip(clip);
+
+    this.clips.forEach((el, idx) => {
+      if (el.docID == clip.docID) {
+        this.clips.splice(idx, 1);
+      }
+    });
   }
 }
